2b77a1900ade2b35a03aaac2a69401729cfc3dc9
easton57 <65976231+easton57@users.noreply.github.com>
2021-02-28 16:00:53 -0700

    Created a method to stack the frames instead of creating new windows for each page, created classes for the login page and change password page, put all gui variables into a class file, created a class to handle the user data, created a class to handle global variables using JSON.

---
+++ Eastons Files/UserData.py
@@ -0,0 +1,72 @@
+"""
+Class file to import and edit user data
+"""
+
+import pandas as pd
+
+
+class UserData:
+
+    # Load the employee csv
+    def __init__(self):
+        self.employee_data = pd.read_csv("employees.csv")
+
+    # Method for setting a new password
+    def change_field(self, emp_num, new_value, field):
+        self.employee_data.loc[self.employee_data["id"] == emp_num, field] = new_value
+        self.employee_data.to_csv("employees.csv")
+
+    def verify_user(self, emp_num, password):
+        read_password = self.employee_data.loc[self.employee_data["id"] == emp_num, "password"]
+
+        # find the password in the gross csv data type
+        for i in read_password:
+            if type(i) != str:
+                continue
+            else:
+                read_password = i
+
+        if read_password == 'None':
+            return read_password
+        elif read_password == password:
+            return True
+        else:
+            return False
+
+    def read_value(self, emp_num, field):
+        value = self.employee_data.loc[self.employee_data["id"] == emp_num, field]
+
+        # find the value in the gross csv data type
+        for i in value:
+            if type(i) != str:
+                continue
+            else:
+                value = i
+
+        return value
+
+    def user_exists(self, emp_num):
+        # Use the users last name to see if the exist
+        last_name = self.employee_data.loc[self.employee_data["id"] == emp_num, 'last_name']
+
+        if last_name.empty:
+            return False
+
+        return True
+
+
+"""
+Valid fields are:
+id
+first_name
+last_name
+address
+city
+state
+zip
+classification
+salary
+commission
+hourly
+password
+"""
