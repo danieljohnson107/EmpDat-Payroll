38d0b1864a1cb2dc8e852b46c381d1e87bec496b
Easton Seidel <eseidel1357@gmail.com>
2021-03-15 12:24:47 -0600

    Created a new account function. Had to separate to two separate programs (One for authentication, the other for other functions) due to some issues with importing and using user data. Fixed issue with csv creating a new column after every run.

--- Experiments/MyProfile.py
+++ Experiments/MyProfile.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 # from PIL import imageTk, Image
 from GuiValues import *
+import GlobalVariables as globe
+from tkinter import messagebox
 
 ''' to use images yoy need to install pillow
 install with "pip install pillow" on your python to use
@@ -23,109 +25,92 @@ class MyProfile(Frame):
         Frame.__init__(self, parent)
         self.controller = controller
 
-        gv = GuiValues(self, controller)
-        gv.create_nav_bar()
+        self.gv = GuiValues(self, controller)
+        self.gv.create_nav_bar()
 
         # Buttons
-        gv.employeesButton.place(x=0, y=0)
-        gv.timeCardsButton.place(x=185, y=0)
-        gv.salesButton.place(x=370, y=0)
-        gv.payrollButton.place(x=555, y=0)
-        gv.myProfileButton.place(x=740, y=0)
-        gv.saveProfileButton.place(x=0, y=40)
+        self.gv.employeesButton.place(x=0, y=0)
+        self.gv.timeCardsButton.place(x=185, y=0)
+        self.gv.salesButton.place(x=370, y=0)
+        self.gv.payrollButton.place(x=555, y=0)
+        self.gv.myProfileButton.place(x=740, y=0)
+        self.gv.saveProfileButton.place(x=0, y=40)
+
+        # Define save profile button
+        self.gv.saveProfileButton.config(command=self.save_profile)
 
         # Labels
-        gv.fNameLabel.place(x=200, y=50)
-        gv.lNameLabel.place(x=200, y=75)
-        gv.addressLabel.place(x=200, y=100)
-        gv.addressLineTwoLabel.place(x=200, y=125)
-        gv.cityLabel.place(x=200, y=150)
-        gv.stateLabel.place(x=200, y=175)
-        gv.zipCodeLabel.place(x=200, y=200)
-        gv.phoneLabel.place(x=200, y=225)
-        gv.classificationLabel.place(x=200, y=250)
-        gv.empNumberLabel.place(x=200, y=275)
-        gv.passwordLabel.place(x=200, y=300)
-        gv.departmentLabel.place(x=200, y=325)
-        gv.payRateLabel.place(x=200, y=350)
-        gv.payYTDLabel.place(x=200, y=375)
-        gv.securityAccessLabel.place(x=200, y=400)
+        self.gv.fNameLabel.place(x=200, y=50)
+        self.gv.lNameLabel.place(x=200, y=75)
+        self.gv.addressLabel.place(x=200, y=100)
+        self.gv.addressLineTwoLabel.place(x=200, y=125)
+        self.gv.cityLabel.place(x=200, y=150)
+        self.gv.stateLabel.place(x=200, y=175)
+        self.gv.zipCodeLabel.place(x=200, y=200)
+        self.gv.phoneLabel.place(x=200, y=225)
+        self.gv.classificationLabel.place(x=200, y=250)
+        self.gv.empNumberLabel.place(x=200, y=275)
+        self.gv.passwordLabel.place(x=200, y=300)
+        self.gv.departmentLabel.place(x=200, y=325)
+        self.gv.payRateLabel.place(x=200, y=350)
+        self.gv.payYTDLabel.place(x=200, y=375)
+        self.gv.securityAccessLabel.place(x=200, y=400)
 
         # Inputs
-        gv.fNameInput.place(x=340, y=50, width=gv.inputWidth, height=gv.inputHeight)
-        gv.lNameInput.place(x=340, y=75, width=gv.inputWidth, height=gv.inputHeight)
-        gv.addressInput.place(x=340, y=100, width=gv.inputWidth, height=gv.inputHeight)
-        gv.addressTwoInput.place(x=340, y=125, width=gv.inputWidth, height=gv.inputHeight)
-        gv.cityInput.place(x=340, y=150, width=gv.inputWidth, height=gv.inputHeight)
-        gv.stateInput.place(x=340, y=175, width=gv.inputWidth, height=gv.inputHeight)
-        gv.zipInput.place(x=340, y=200, width=gv.inputWidth, height=gv.inputHeight)
-        gv.phoneInput.place(x=340, y=225, width=gv.inputWidth, height=gv.inputHeight)
-        gv.classInput.place(x=340, y=250, width=gv.inputWidth, height=gv.inputHeight)
-        gv.empNumInput.place(x=340, y=275, width=gv.inputWidth, height=gv.inputHeight)
-        gv.passwordInput.place(x=340, y=300, width=gv.inputWidth, height=gv.inputHeight)
-        gv.departmentInput.place(x=340, y=325, width=gv.inputWidth, height=gv.inputHeight)
-        gv.payRateInput.place(x=340, y=350, width=gv.inputWidth, height=gv.inputHeight)
-        gv.payYTDInput.place(x=340, y=375, width=gv.inputWidth, height=gv.inputHeight)
-        gv.securityInput.place(x=340, y=400, width=gv.inputWidth, height=gv.inputHeight)
+        self.gv.fNameInput.place(x=340, y=50, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.lNameInput.place(x=340, y=75, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.addressInput.place(x=340, y=100, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.addressTwoInput.place(x=340, y=125, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.cityInput.place(x=340, y=150, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.stateInput.place(x=340, y=175, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.zipInput.place(x=340, y=200, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.phoneInput.place(x=340, y=225, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.classInput.place(x=340, y=250, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.empNumInput.place(x=340, y=275, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.passwordInput.place(x=340, y=300, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.departmentInput.place(x=340, y=325, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.payRateInput.place(x=340, y=350, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.payYTDInput.place(x=340, y=375, width=self.gv.inputWidth, height=self.gv.inputHeight)
+        self.gv.securityInput.place(x=340, y=400, width=self.gv.inputWidth, height=self.gv.inputHeight)
 
-        emp_id = globe.ud.employee_number
+        # fill the fields
+        self.gv.my_profile_values()
 
-        # Clear all fields in case of double click
-        gv.fNameInput.delete(0, "end")
-        gv.fNameInput.delete(0, "end")
-        gv.lNameInput.delete(0, "end")
-        gv.addressInput.delete(0, "end")
-        gv.addressTwoInput.delete(0, "end")
-        gv.cityInput.delete(0, "end")
-        gv.stateInput.delete(0, "end")
-        gv.zipInput.delete(0, "end")
-        gv.phoneInput.delete(0, "end")
-        gv.classInput.delete(0, "end")
-        gv.empNumInput.delete(0, "end")
-        gv.passwordInput.delete(0, "end")
-        gv.departmentInput.delete(0, "end")
-        gv.payRateInput.delete(0, "end")
-        gv.payYTDInput.delete(0, "end")
-        gv.securityInput.delete(0, "end")
+    def save_profile(self):
 
-        # Insert values from CSV
-        gv.fNameInput.insert(0, globe.ud.read_value(emp_id, "first_name"))
-        gv.lNameInput.insert(0, globe.ud.read_value(emp_id, "last_name"))
-        gv.addressInput.insert(0, globe.ud.read_value(emp_id, "address"))
-        # self.addressTwoInput.insert(0, ud.read_value(emp_id, ""))
-        gv.cityInput.insert(0, globe.ud.read_value(emp_id, "city"))
-        gv.stateInput.insert(0, globe.ud.read_value(emp_id, "state"))
-        gv.zipInput.insert(0, globe.ud.read_value(emp_id, "zip"))
-        # self.phoneInput.insert(0, ud.read_value(emp_id, ""))
-        gv.classInput.insert(0, globe.ud.read_value(emp_id, "classification"))
-        gv.empNumInput.insert(0, globe.ud.read_value(emp_id, "id"))
-        gv.passwordInput.insert(0, globe.ud.read_value(emp_id, "password"))
-        # self.departmentInput.insert(0, ud.read_value(emp_id, ""))
-        gv.payRateInput.insert(0, globe.ud.read_value(emp_id, "hourly"))
-        gv.payYTDInput.insert(0, globe.ud.read_value(emp_id, "salary"))
-        gv.securityInput.insert(0, globe.ud.read_value(emp_id, "access"))
+        # Grab the new info
+        fname = self.gv.fname_var.get()
+        lname = self.gv.lname_var.get()
+        address = self.gv.address_var.get()
+        address2 = self.gv.address2_var.get()
+        city = self.gv.city_var.get()
+        state = self.gv.state_var.get()
+        zip = self.gv.zip_var.get()
+        phone = self.gv.phone_var.get()
+        classification = self.gv.class_var.get()
+        emp_num = self.gv.emp_num_var.get()
+        password = self.gv.passw_var.get()
+        department = self.gv.department_var.get()
+        pay_rate = self.gv.pay_rate_var.get()
+        pay_ytd = self.gv.pay_ytd_var.get()
+        security = self.gv.security_var.get()
 
-    def pay(self):
-        self.controller.show_frame("PayrollProcessing")
+        # Set the new values
+        globe.ud.change_field(globe.ud.employee_number, fname, 'first_name')
+        globe.ud.change_field(globe.ud.employee_number, lname, 'last_name')
+        globe.ud.change_field(globe.ud.employee_number, address, 'address')
+        globe.ud.change_field(globe.ud.employee_number, address2, 'address2')
+        globe.ud.change_field(globe.ud.employee_number, city, 'city')
+        globe.ud.change_field(globe.ud.employee_number, state, 'state')
+        globe.ud.change_field(globe.ud.employee_number, zip, 'zip')
+        globe.ud.change_field(globe.ud.employee_number, phone, 'phone_number')
+        globe.ud.change_field(globe.ud.employee_number, classification, 'classification')
+        globe.ud.change_field(globe.ud.employee_number, password, 'password')
+        globe.ud.change_field(globe.ud.employee_number, department, 'department')
+        globe.ud.change_field(globe.ud.employee_number, pay_rate, 'hourly')
+        globe.ud.change_field(globe.ud.employee_number, pay_ytd, 'salary')
+        globe.ud.change_field(globe.ud.employee_number, security, 'access')
+        globe.ud.change_field(globe.ud.employee_number, emp_num, 'id')
 
-    def employees(self):
-        self.controller.show_frame("FindEmployee")
-
-    def timecards(self):
-        pass
-
-    def sales(self):
-        pass
-
-    def myProfile(self):
-        self.controller.show_frame("MyProfile")
-
-    def saveChanges(self):
-        pass
-
-    ''' last thing is create an event loop to watch
-
-    the interface track location of mouse '''
-
-    '''get buttons to do something
-    create function '''
+        # Pop up letting them know that it updated successfully
+        messagebox.showinfo("Success!", "Successfully Updated")
